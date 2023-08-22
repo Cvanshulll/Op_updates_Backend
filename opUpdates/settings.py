adb575e01009e065eb597327b9bd849a0f91911d
Vanshul Choudhary <cvanshul09@gmail.com>
2023-08-22 20:02:30 +0530

    pushed all changes

--- opUpdates/settings.py
+++ opUpdates/settings.py
@@ -128,12 +128,12 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'cvanshul09@gmail.com'
-EMAIL_HOST_PASSWORD = 'van.Mansi'
+# EMAIL_HOST_USER = '***@gmail.com'
+# EMAIL_HOST_PASSWORD = '***'
 
 
 
